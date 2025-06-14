dbcbe548be91576c2075ab4060c124a2fc0c4947
jprivas30 <juanpr2512@gmail.com>
2025-06-13 21:56:47 -0400

    Practica SonarQube

---
+++ src/main/java/com/equipo/validador/App.java
@@ -0,0 +1,13 @@
+package com.equipo.validador;
+
+public class App {
+    public static void main(String[] args) {
+        String usuario = "admin";
+        String contraseña = "123456"; // Hardcoded credentials
+        if (usuario.equals("admin")) {
+            System.out.println("¡Bienvenido administrador!");
+        }
+    }
+}
+
+
